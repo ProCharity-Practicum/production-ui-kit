819a80b3eee7762f6effd341c8fba658e61b5f16
Andrey68Vyazovov <139168951+Andrey68Vyazovov@users.noreply.github.com>
2025-04-24 08:15:22 +0300

    fix: close on overlay click with state update

--- src/components/Common/Layout/Modal/Modal.tsx
+++ src/components/Common/Layout/Modal/Modal.tsx
@@ -1,20 +1,49 @@
 import clsx from 'clsx';
 import styles from './Modal.module.scss';
 import { Icon } from '@/components/Core/Icon';
+import { useEffect } from 'react';
 
 export type ModalProps = {
 	className?: string;
 	onClose: () => void;
 	children: React.ReactNode;
+	closeOnOverlayClick?: boolean;
 };
 
-export function Modal({ onClose, children, className }: ModalProps) {
+export function Modal({
+	onClose,
+	children,
+	className,
+	closeOnOverlayClick,
+}: ModalProps) {
+	useEffect(() => {
+		if (!closeOnOverlayClick) return; // Если закрытие по клику отключено, Escape тоже игнорируем
+
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') onClose();
+		};
+
+		document.addEventListener('keydown', handleKeyDown);
+		return () => document.removeEventListener('keydown', handleKeyDown);
+	}, [onClose, closeOnOverlayClick]);
+
+	const handleOverlayClick = (e: React.MouseEvent) => {
+		if (closeOnOverlayClick) {
+			onClose();
+		}
+		e.stopPropagation();
+	};
+
+	const handleModalClick = (e: React.MouseEvent) => {
+		e.stopPropagation(); // блокируем всплытие клика из модалки
+	};
+
 	return (
 		<div
 			className={clsx(styles.overlay, className)}
-			onClick={(e) => e.stopPropagation()}
+			onClick={handleOverlayClick}
 		>
-			<div className={styles.modal}>
+			<div className={styles.modal} onClick={handleModalClick}>
 				<button className={clsx(styles.icon, className)} onClick={onClose}>
 					<Icon name="close" />
 				</button>
